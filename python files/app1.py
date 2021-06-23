5c4ecb8e9cc35e292cde87215c4a6a7c165040eb
rachel22thomas <rachelthomas2212@gmail.com>
2021-06-23 21:28:31 +0530

    first commit

---
+++ python files/app1.py
@@ -0,0 +1,122 @@
+from flask import Flask,redirect, url_for,render_template,request,session
+import os
+from index import d_dtcn
+import re
+import MySQLdb.cursors
+from flask_mysqldb import MySQL
+
+secret_key = str(os.urandom(24))
+
+app = Flask(__name__)
+app.config['TESTING'] = True
+app.config['DEBUG'] = True
+app.config['FLASK_ENV'] = 'development'
+app.config['SECRET_KEY'] = secret_key
+app.config['DEBUG'] = True
+
+# Defining the home page of our site
+@app.route("/",methods=['GET', 'POST'])
+def home():
+    print(request.method)
+    if request.method == 'POST':
+        if request.form.get('Continue') == 'Continue':
+           return render_template("index1.html")
+    else:
+        # pass # unknown
+        return render_template("index.html")
+
+@app.route("/start", methods=['GET', 'POST'])
+def index():
+    print(request.method)
+    if request.method == 'POST':
+        if request.form.get('Start') == 'Start':
+            # pass
+            d_dtcn()
+            return render_template("index.html")
+    else:
+        # pass # unknown
+        return render_template("index.html")
+
+@app.route('/contact', methods=['GET', 'POST'])
+def cool_form():
+    if request.method == 'POST':
+        # do stuff when the form is submitted
+
+        # redirect to end the POST handling
+        # the redirect can be to the same route or somewhere else
+        return redirect(url_for('index'))
+
+    # show the form, it wasn't submitted
+    return render_template('contact.html')
+# --------------------------------------------------------------
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'reuben52'
+app.config['MYSQL_DB'] = 'geeklogin'
+
+mysql = MySQL(app)
+
+@app.route('/')
+@app.route('/login', methods =['GET', 'POST'])
+def login():
+	msg = ''
+	if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+		username = request.form['username']
+		password = request.form['password']
+		cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+		cursor.execute('SELECT * FROM accounts WHERE username = % s AND password = % s', (username, password, ))
+		account = cursor.fetchone()
+		if account:
+			session['loggedin'] = True
+			session['id'] = account['id']
+			session['username'] = account['username']
+			msg = 'Logged in successfully !'
+			return render_template('index1.html', msg = msg)
+		else:
+			msg = 'Incorrect username / password !'
+	return render_template('login.html', msg = msg)
+
+@app.route('/logout')
+def logout():
+	session.pop('loggedin', None)
+	session.pop('id', None)
+	session.pop('username', None)
+	return redirect(url_for('index'))
+
+@app.route('/register', methods =['GET', 'POST'])
+def register():
+	msg = ''
+	if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form :
+		username = request.form['username']
+		password = request.form['password']
+		email = request.form['email']
+		cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+		cursor.execute('SELECT * FROM accounts WHERE username = % s', (username, ))
+		account = cursor.fetchone()
+		if account:
+			msg = 'Account already exists !'
+		elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+			msg = 'Invalid email address !'
+		elif not re.match(r'[A-Za-z0-9]+', username):
+			msg = 'Username must contain only characters and numbers !'
+		elif not username or not password or not email:
+			msg = 'Please fill out the form !'
+		else:
+			cursor.execute('INSERT INTO accounts VALUES (NULL, % s, % s, % s)', (username, password, email, ))
+			mysql.connection.commit()
+			msg = 'You have successfully registered !'
+	elif request.method == 'POST':
+		msg = 'Please fill out the form !'
+	return render_template('register.html', msg = msg)
+
+
+
+
+
+
+
+# --------------------------------------------------------------------------
+if __name__ == "__main__":
+    app.run()
+    
